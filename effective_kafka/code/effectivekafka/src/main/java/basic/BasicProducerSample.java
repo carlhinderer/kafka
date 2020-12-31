f4e653564e43a061506a0f6c067ebf3fd2c1cec8
Carl Hinderer <carl.hinderer4@gmail.com>
2020-12-30 23:07:30 -0700

    Added example of simple Java producer.

---
+++ effective_kafka/code/effectivekafka/src/main/java/basic/BasicProducerSample.java
@@ -0,0 +1,41 @@
+package basic;
+
+import static java.lang.System.*;
+
+import java.util.*;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.*;
+
+
+public final class BasicProducerSample {
+
+  public static void main(String[] args) throws InterruptedException {
+    final var topic = "getting-started";
+
+    final Map<String, Object> config = 
+        Map.of(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092", 
+               ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName(), 
+               ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName(), 
+               ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+
+    try (var producer = new KafkaProducer<String, String>(config)) {
+      while (true) {
+        final var key = "myKey";
+        final var value = new Date().toString();
+        out.format("Publishing record with value %s%n", value);
+
+        final Callback callback = (metadata, exception) -> {
+          out.format("Published with metadata: %s, error: %s%n", 
+                     metadata, exception);
+        };
+
+        // publish the record, handling the metadata in the callback
+        producer.send(new ProducerRecord<>(topic, key, value), callback);
+
+        // wait a second before publishing another
+        Thread.sleep(1000);
+      }
+    }
+  }
+}
